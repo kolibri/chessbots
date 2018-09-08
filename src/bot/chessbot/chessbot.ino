7860b2823231f147b79906a26c7ddcb773628978
ko <entengelb@gmail.com>
2018-09-08 19:48:53 +0200

    bot prototype2

--- src/bot/chessbot/chessbot.ino
+++ src/bot/chessbot/chessbot.ino
@@ -1,19 +1,22 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "Trochilidae";
 const char* password = "humm!ngb!rd31";
 
 #define MOTOR1_A D6
 #define MOTOR1_B D8
-#define MOTOR1_PWM D5
+#define MOTOR1_PWM 14
 #define MOTOR2_A D1
 #define MOTOR2_B D4
-#define MOTOR2_PWM D2
+#define MOTOR2_PWM 4
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
 
 void setup() {
     Serial.begin(115200);
+
     pinMode(MOTOR1_A, OUTPUT);
     pinMode(MOTOR1_B, OUTPUT);
     pinMode(MOTOR1_PWM, OUTPUT);
@@ -23,10 +26,10 @@ void setup() {
 
     digitalWrite(MOTOR1_A, LOW);
     digitalWrite(MOTOR1_B, LOW);
-    //analogWrite(MOTOR1_PWM, 255);
+    analogWrite(MOTOR1_PWM, 127);
     digitalWrite(MOTOR2_A, LOW);
     digitalWrite(MOTOR2_B, LOW);
-    //analogWrite(MOTOR2_PWM, 255);
+    analogWrite(MOTOR2_PWM, 127);
 
 
     Serial.print("Connecting to ");
@@ -37,79 +40,56 @@ void setup() {
         delay(500);
         Serial.print(".");
     }
+    
+    server.on("/sequence", handleRequest); 
 
     server.begin();
     Serial.println("Wifi connected & Server started");
 
     Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
-    WiFiClient client = server.available();
-    if (!client) {
+    server.handleClient();
+}
+
+void handleRequest() {
+      if (server.hasArg("plain")== false){ //Check if body received
+        server.send(400, "text/plain", "Client Error: Empty body");
+
         return;
     }
 
-    Serial.println("new client");
-    while(!client.available()){
-        delay(1);
-    }
+    StaticJsonBuffer<200> jsonBuffer;
 
-    String req = client.readStringUntil('\r');
-    Serial.println(req);
-    client.flush();
-    if (req.indexOf("/off") != -1) {
-        digitalWrite(MOTOR1_A, LOW);
-        digitalWrite(MOTOR1_B, LOW);
-        digitalWrite(MOTOR2_A, LOW);
-        digitalWrite(MOTOR2_B, LOW);
-    } else if (req.indexOf("/0:0")     != -1) {
-        controlMotor(0,   MOTOR1_A, MOTOR1_B, MOTOR1_PWM);
-        controlMotor(0,   MOTOR2_A, MOTOR2_B, MOTOR2_PWM);
-    } else if (req.indexOf("/64:64")   != -1) {
-        controlMotor(64,  MOTOR1_A, MOTOR1_B, MOTOR1_PWM);
-        controlMotor(64,  MOTOR2_A, MOTOR2_B, MOTOR2_PWM);
-    } else if (req.indexOf("/127:127") != -1) {
-        controlMotor(127, MOTOR1_A, MOTOR1_B, MOTOR1_PWM);
-        controlMotor(127, MOTOR2_A, MOTOR2_B, MOTOR2_PWM);
-    } else if (req.indexOf("/192:192") != -1) {
-        controlMotor(192, MOTOR1_A, MOTOR1_B, MOTOR1_PWM);
-        controlMotor(192, MOTOR2_A, MOTOR2_B, MOTOR2_PWM);
-    } else if (req.indexOf("/255:255") != -1) {
-        controlMotor(255, MOTOR2_A, MOTOR2_B, MOTOR2_PWM);
-        controlMotor(255, MOTOR1_A, MOTOR1_B, MOTOR1_PWM);
-    } else {
-        Serial.println("invalid request");
-        client.stop();
+    JsonObject& sequence = jsonBuffer.parseObject(server.arg("plain"));
+
+    if (!sequence.success()) {
+        Serial.println("parseObject() failed");
         return;
-    }
+    }      
 
-    client.flush();
+    int r = sequence["r"];
+    int l = sequence["l"];
+    int t = sequence["t"];
 
-// Prepare the response
-    String s = "HTTP/1.1 200 OK\r\n";
-    s += "Content-Type: application/json\r\n\r\n";
-    s += "{\"state\": \"ok\"}\n";
+    controlMotor(r, MOTOR2_A, MOTOR2_B, MOTOR2_PWM);
+    controlMotor(l, MOTOR1_A, MOTOR1_B, MOTOR1_PWM);
 
-// Send the response to the client
-    client.print(s);
-    delay(1);
-    Serial.println("Client disonnected");
-
-// The client will actually be disconnected 
-// when the function returns and 'client' object is detroyed
+    server.send(200, "text/plain", "ok");
 }
 
 void controlMotor(int val, int ma, int mb, int mpwm) {
-    //analogWrite(mpwm,val);
+    analogWrite(mpwm,abs(val));
     digitalWrite(ma,LOW);
     digitalWrite(mb,LOW);
 
-    if (127 < val) {
+    if (0 < val) {
         digitalWrite(ma,HIGH);
     }
 
-    if (127 > val) {
+    if (0 > val) {
         digitalWrite(mb,HIGH);
     }
 }
